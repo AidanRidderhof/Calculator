abf9e1706e031058340a5d63748cb32ed45a3b0b
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2025-02-12 17:50:07 -0500

    fix mult error

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function operate(num1, num2, op) {
     switch (op) {
         case '+': return num1 + num2;
         case '-': return num1 - num2;
-        case '*': return num1 * num2;
+        case 'x': return num1 * num2;
         case '/': return num2 !== 0 ? num1 / num2 : "Error";
         default: return "Invalid Operation";
     }
