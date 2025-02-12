f85ed46116fbb16e5ac66b5c49ba435aad0c3597
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2025-02-12 17:16:03 -0500

    changing logic

--- javascript.js
+++ javascript.js
@@ -33,21 +33,28 @@ const nums = document.getElementsByClassName("num");
 const operators = document.getElementsByClassName("operator");
 const clear = document.getElementById("clear");
 let display = document.getElementById("display");
+
+//operators append display
+for (let i=0; i<operators.length; i++){
+    operators[i].addEventListener('click', function() {
+        let op = this.innerText;
+        let num1 = display.innerText;
+        num1 = +num1;
+        
+    })
+}
+
 //numbers append display
 for (let i=0; i<nums.length; i++){
     nums[i].addEventListener('click', function() {
+        if (op!=null) {
+            display.innerText='';
+        }
         let text = this.innerText;
         display.innerText += text;
     })
 }
 
-//operators append display
-for (let i=0; i<operators.length; i++){
-    operators[i].addEventListener('click', function() {
-        let text = this.innerText;
-        display.innerText += " "+text+" ";
-    })
-}
 
 //clear button works
 clear.addEventListener('click', function() {
